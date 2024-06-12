973660f9cbad2cd73829fd6114722410252a28a0
itscsp <chethanspoojary1@gmail.com>
2024-06-12 21:59:56 +0530

    Addin env file configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+dotenv.config(); // load env vars from .env
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __BASE_URL__: `"${process.env.BASE_URL}"`,
+  },
 })
